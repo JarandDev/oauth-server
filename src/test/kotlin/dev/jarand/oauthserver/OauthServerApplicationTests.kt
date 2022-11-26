378e44d874bc0dc79fffa4aeea410023069eaf99
JarandDev <jarand95@gmail.com>
2022-11-26 12:17:15 +0100

    Initialize project

---
+++ src/test/kotlin/dev/jarand/oauthserver/OauthServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.jarand.oauthserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
